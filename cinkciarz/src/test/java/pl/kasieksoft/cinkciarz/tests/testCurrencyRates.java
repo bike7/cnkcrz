1e27170d3b2d6490b96a4669ab10e9d1dc73fa45
Katarzyna Pytel <pytel.kat@gmail.com>
2019-10-13 20:34:18 +0200

    rebased commit

--- cinkciarz/src/test/java/pl/kasieksoft/cinkciarz/tests/testCurrencyRates.java
+++ cinkciarz/src/test/java/pl/kasieksoft/cinkciarz/tests/testCurrencyRates.java
@@ -21,6 +21,7 @@ public class testCurrencyRates {
         start();
         System.out.println(readCurrencyRate("chf/pln"));
         System.out.println(readCurrencyRate("eur/pln"));
+        System.out.println(readCurrencyRate("eur/chf"));
         System.out.println(readCurrencyRate("eur/usd"));
         stop();
     }
